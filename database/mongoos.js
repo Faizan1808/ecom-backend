4285d708bc220718220627373c424394a044da88
Faizan Ansari <88032699+Faizan1808@users.noreply.github.com>
2023-04-26 17:26:54 +0530

    Update mongoos.js

--- database/mongoos.js
+++ database/mongoos.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connection = () => mongoose.connect("mongodb+srv://faiz63440:Faizan@1808@cluster0.ubh59hd.mongodb.net/?retryWrites=true&w=majority")
+const connection = () => mongoose.connect("mongodb+srv://faiz63440:Faizan@1808@cluster0.ubh59hd.mongodb.net/Ecommerce?retryWrites=true&w=majority")
 
 .then((res)=>{
     (res)
